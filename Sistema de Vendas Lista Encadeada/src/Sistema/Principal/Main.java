0e8adfec23dd9c85c6cdc04ff4c036709ddcc426
Adrian-Batista <62628581+Adrian-Batista@users.noreply.github.com>
2020-09-09 13:18:09 -0300

    Atualização

--- Sistema de Vendas Lista Encadeada/src/Sistema/Principal/Main.java
+++ Sistema de Vendas Lista Encadeada/src/Sistema/Principal/Main.java
@@ -1,10 +1,51 @@
 package Sistema.Principal;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int opcao = 0;
+		
+		Scanner entrada = new Scanner(System.in);
+		System.out.println("\n\n BEM VINDO AO YOUTUBE ");	
+		System.out.println("\n================================= \n|\t\t\t\t|");
+		System.out.println("| ( 1 ) - ADICIONAR VENDA       | \n|\t\t\t\t|");
+		System.out.println("| ( 2 ) - CONSULTAR VENDA       | \n|\t\t\t\t|");
+		System.out.println("| ( 3 ) - ATUALIZAR VENDA       | \n|\t\t\t\t| ");
+		System.out.println("| ( 4 ) - REMOVER VENDA         | \n|\t\t\t\t| ");
+		System.out.println("| ( 0 ) - SAIR                  | \n|\t\t\t\t|");
+		System.out.println("=================================\n");
 
+		System.out.println(" Escolha uma das opções: ");
+		opcao = entrada.nextInt();
+		entrada.nextLine();
+		System.out.print("\n");
+		
+		switch(opcao) {
+		
+		case 1:
+			break;
+		case 2:
+			break;
+			
+		case 3:
+			break;
+			
+		case 4:
+			break;
+			
+		case 0:
+			break;
+		
+		}
+		
+		
+		
+		
+		
+		
+		entrada.close();
 	}
 
 }
