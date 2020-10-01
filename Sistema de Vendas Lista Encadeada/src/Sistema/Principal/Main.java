67d0d52ad935bf4ab98812e8edd3be69285aee8c
Adrian-Batista <62628581+Adrian-Batista@users.noreply.github.com>
2020-10-01 15:50:54 -0300

    Aplicando Correções

--- Sistema de Vendas Lista Encadeada/src/Sistema/Principal/Main.java
+++ Sistema de Vendas Lista Encadeada/src/Sistema/Principal/Main.java
@@ -3,7 +3,7 @@ package Sistema.Principal;
 import java.io.IOException;
 import java.util.Scanner;
 
-import Sistema.Entidades.Produto;
+import Sistema.Entidades.EncadeadaProduto;
 
 public class Main {
 
@@ -15,9 +15,9 @@ public class Main {
 			Scanner entrada = new Scanner(System.in);
 			System.out.println("\n\n BEM VINDO AO YOUTUBE ");	
 			System.out.println("\n================================= \n|\t\t\t\t|");
-			System.out.println("| ( 1 ) - MENU DE PRODUTOS       | \n|\t\t\t\t|");
+			System.out.println("| ( 1 ) - MENU DE PRODUTOS      | \n|\t\t\t\t|");
 			System.out.println("| ( 2 ) - ADICIONAR VENDA       | \n|\t\t\t\t|");
-			System.out.println("| ( 3 ) - LISTAR VENDAS       | \n|\t\t\t\t| ");
+			System.out.println("| ( 3 ) - LISTAR VENDAS         | \n|\t\t\t\t| ");
 			System.out.println("| ( 0 ) - SAIR                  | \n|\t\t\t\t|");
 			System.out.println("=================================\n");
 
@@ -32,25 +32,34 @@ public class Main {
 				int opcao2 = 0;
 				do {
 					Main.LimparTela();
-					Scanner entrada = new Scanner(System.in);
-					System.out.println("\n\n BEM VINDO AO YOUTUBE ");	
+					System.out.println("\n\n-------------- MENU DE PRODUTOS --------------- ");	
 					System.out.println("\n================================= \n|\t\t\t\t|");
-					System.out.println("| ( 1 ) - MENU DE PRODUTOS       | \n|\t\t\t\t|");
-					System.out.println("| ( 2 ) - ADICIONAR VENDA       | \n|\t\t\t\t|");
-					System.out.println("| ( 3 ) - LISTAR VENDAS       | \n|\t\t\t\t| ");
+					System.out.println("| ( 1 ) - LISTAR PRODUTOS       | \n|\t\t\t\t|");
+					System.out.println("| ( 2 ) - ADICIONAR PRODUTO     | \n|\t\t\t\t|");
+					System.out.println("| ( 3 ) - ATUALIZAR PRODUTO     | \n|\t\t\t\t| ");
+					System.out.println("| ( 4 ) - REMOVER PRODUTO       | \n|\t\t\t\t| ");
 					System.out.println("| ( 0 ) - SAIR                  | \n|\t\t\t\t|");
 					System.out.println("=================================\n");
 
 					System.out.println(" Escolha uma das opções: ");
-					opcao = entrada.nextInt();
+					opcao2 = entrada.nextInt();
 					entrada.nextLine();
 					System.out.print("\n");
 
 					switch(opcao2) {
 
-
+					case 1:
+						EncadeadaProduto.listarElementos();
+						break;
+						
+					case 2:
+						break;
+					
+					case 3:
+						break;
 
 					}
+					
 				}while (opcao2 != 0);
 
 
@@ -72,9 +81,9 @@ public class Main {
 				System.in.read();
 
 			}
-
+			
 		}while (opcao != 0);
-
+		
 	}
 
 
